c301681ff843b16287412b5cd37ca637af3a68e3
Ilyas AKTURAN <akturanilyas@gmail.com>
2023-06-15 22:54:06 +0300

    Fix test config

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,5 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  testEnvironment: 'jsdom',
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  testTimeout: 10000,
-  resetMocks: false,
   bail: 1,
   verbose: true,
 };
